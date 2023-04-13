915c18c96308c6456e2900dab44fa000c5f56c31
falfeya <130572121+falfeya@users.noreply.github.com>
2023-04-13 13:10:28 +0500

    Add files via upload

---
+++ практич/Program.cs
@@ -0,0 +1,25 @@
+﻿string fullName = "Иванов Иван Иванович";
+int age = 20;
+string mail = "ivan@mail.ru";
+double program = 8.9;
+double math = 7.1;
+double phys = 9.5;
+Console.WriteLine($"Ф.И.О: {fullName}");
+Console.WriteLine($"Возраст: {age}");
+Console.WriteLine($"Почта: {mail}");
+Console.WriteLine($"Баллы по программированию: {program}");
+Console.WriteLine($"Баллы по математике: {math}");
+Console.WriteLine($"Баллы по физике: {phys}");
+
+double p = 8.9;
+double m = 7.1;
+double ph = 9.5;
+double sum = p + m + ph;
+Console.WriteLine($"Сумма баллов: {sum}");
+
+double p1 = 8.9;
+double m2 = 7.1;
+double ph3 = 9.5;
+double avg = (p1 + m2 + ph3)/3;
+Console.WriteLine($"Среднее арифметическое: {avg}");
+
